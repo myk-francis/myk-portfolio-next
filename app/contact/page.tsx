bb22e3cf4fae54a594195eab9ee10efddab55a51
myk <michaelmugendi1707@gmail.com>
2023-07-03 08:03:37 +0300

    made a few changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+import { fadeIn } from "@/variants";
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="h-full bg-primary/30 ">
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        {/* text and form*/}
+        <div className="flex flex-col w-full max-w-[700px]">
+          {/* text  */}
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit={"hidden"}
+            className="h2 mb-12 text-center"
+          >
+            Let`&apos;`s <span className="text-accent">Connect.</span>
+          </motion.h2>
+          {/* form  */}
+          <motion.form
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit={"hidden"}
+            className="flex flex-1 flex-col gap-6 w-full mx-auto"
+          >
+            {/* input group */}
+            <div className="flex gap-x-6 w-full">
+              <input type="text" placeholder="name" className="input" />
+              <input type="email" placeholder="email" className="input" />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea placeholder="message" className="textarea"></textarea>
+            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all flex items-center justify-center overflow-hidden hover:border-accent group">
+              <span className="group-hover:translate-y-[120%] group-hover:opacity-0 transition-all duration-300">
+                Lets Talk
+              </span>
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+            </button>
+          </motion.form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
